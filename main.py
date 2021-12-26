eee82e42b83e5720593cc1221cde2dc0d2bc3bf1
raku10ehime <95738593+raku10ehime@users.noreply.github.com>
2021-12-26 22:54:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -160,7 +160,6 @@ if update4G == update_mil == update_sub:
         consumer_secret = os.environ["CONSUMER_SECRET"]
         access_token = os.environ["ACCESS_TOKEN"]
         access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
-        bearer_token = os.environ["BEARER_TOKEN"]
 
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
